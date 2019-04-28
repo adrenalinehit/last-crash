631cf1689f447d36a81309f9068eb9677cd3e59b
Nick Payne <ndpayne@gmail.com>
2019-04-28 18:14:11 +0100

    adding profile pic and strava widget

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          Welcome to <i>Nick last crashed...</i>
+        <img src="https://www.gravatar.com/avatar/0f879f3ea09c6e40a566f1a06f760c3c"/>
+          <div className="floater">
+            Welcome to <i>Nick last crashed...</i>
+          </div>
+          <iframe height='160' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/803880/activity-summary/63119c16f273d385e23041a35d4b09de83926ed0'></iframe>
         </header>
         <Strava />
-        <Images/>
+        <Images />
         <footer className="App-footer">
-          <TimeDisplay/>
+          <TimeDisplay />
         </footer>
       </div>
     );
