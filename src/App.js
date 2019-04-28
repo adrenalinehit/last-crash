daaf8d908cea86b7a6d55bb9608dfb6fedf47c40
Nick Payne <ndpayne@gmail.com>
2019-04-28 18:21:10 +0100

    spacing in header

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <header className="App-header">
         <img src="https://www.gravatar.com/avatar/0f879f3ea09c6e40a566f1a06f760c3c"/>
           <div className="floater">
-            Welcome to <i>Nick last crashed...</i>
+            <i>Nick last crashed...</i>
           </div>
           <iframe height='160' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/803880/activity-summary/63119c16f273d385e23041a35d4b09de83926ed0'></iframe>
         </header>
